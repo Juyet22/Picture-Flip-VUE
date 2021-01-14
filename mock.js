4504b2b935c27baa71da1700b36c3f01a4a88412
Juyet22 <juste654321@163.com>
2021-01-15 00:29:37 +0800

    Picture Flip

---
+++ mock.js
@@ -0,0 +1,33 @@
+let IMAGES = [
+  "./images/Akali.jpg",
+  "./images/Annie.jpg",
+  "./images/Arli.jpg",
+  "./images/Ash.jpg",
+  "./images/Caitlin.jpg",
+  "./images/Fionna.jpg",
+  "./images/Janna.jpg",
+  "./images/Jinx.jpg",
+  "./images/Lissandra.jpg",
+  "./images/Lulu.jpg",
+  "./images/Lux.jpg",
+  "./images/Nami.jpg",
+  "./images/Oriana.jpg",
+  "./images/Riven.jpg",
+  "./images/Riven-2.jpg",
+]
+
+let initialNum = 8
+let randomSize = [1, 2, 3, 4]
+
+function getImageGenerator() {
+  const source = IMAGES.slice()
+  let initial = true
+  return () => {
+    if (initial) {
+      initial = false
+      return source.splice(0, initialNum)
+    }
+    const size = getRandomInt(0, randomSize.length - 1)
+    return source.splice(0, randomSize[size])
+  }
+}
---
+++ util.js
@@ -0,0 +1,42 @@
+let preload = imgs => {
+  return new Promise((resolve, reject) => {
+    if (!imgs.length) {
+      resolve()
+    }
+    const length = imgs.length
+    let count = 0
+
+    const load = (src) => {
+      let img = new Image()
+      const checkIfFinished = () => {
+        // 加载计数
+        count++
+        if (count === length) {
+          resolve()
+        }
+      }
+
+      img.onload = checkIfFinished
+      img.onerror = checkIfFinished
+
+      img.src = src
+    }
+    imgs.forEach(load)
+  })
+}
+
+let getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+let shuffle = (arr) => {
+  let ret = arr.slice()
+  for (let i = 0; i < ret.length; i++) {
+    let j = getRandomInt(0, i)
+    let t = ret[i]
+    ret[i] = ret[j]
+    ret[j] = t
+  }
+  return ret
+}
