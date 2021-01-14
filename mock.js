eb446c46b212452a15f73730e7152bf18fe48acc
Juyet22 <juste654321@163.com>
2021-01-15 01:38:31 +0800

    优化动画效果

--- mock.js
+++ mock.js
@@ -16,18 +16,18 @@ let IMAGES = [
   "./images/Riven-2.jpg",
 ]
 
-let initialNum = 8
-let randomSize = [1, 2, 3, 4]
+let initialNum = 8;
+let randomSize = [1, 2, 3, 4];
 
 function getImageGenerator() {
-  const source = IMAGES.slice()
-  let initial = true
+  const source = IMAGES.slice();
+  let initial = true;
   return () => {
     if (initial) {
-      initial = false
-      return source.splice(0, initialNum)
+      initial = false;
+      return source.splice(0, initialNum);
     }
-    const size = getRandomInt(0, randomSize.length - 1)
-    return source.splice(0, randomSize[size])
+    const size = getRandomInt(0, randomSize.length - 1);
+    return source.splice(0, randomSize[size]);
   }
 }
